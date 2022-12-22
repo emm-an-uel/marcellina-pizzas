3c0f432f0c427339c142b3038ad99a3db08e58e0
emmanuel <sunartio.emmanuel@gmail.com>
2022-12-22 10:59:33 +1030

    initial copied code from navigation drawer

--- app/src/main/java/com/example/marcellinapizzas/MainActivity.kt
+++ app/src/main/java/com/example/marcellinapizzas/MainActivity.kt
@@ -2,20 +2,51 @@ package com.example.marcellinapizzas
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.marcellinapizzas.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    // view binding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
     lateinit var viewModel: ViewModelMain
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarMain.toolbar)
+
+        // setup navigation drawer
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_quiz, R.id.nav_solutions
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
 
         // instantiate viewModel and create list of pizzas
         viewModel = ViewModelProvider(this)[ViewModelMain::class.java]
         viewModel.createListOfPizzas()
+    }
 
-        // test
-        supportFragmentManager.beginTransaction().replace(R.id.nav_host, FragmentQuiz()).commit()
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
