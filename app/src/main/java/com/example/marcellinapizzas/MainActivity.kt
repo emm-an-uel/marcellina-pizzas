81ad563b792116c13632bfb3f7c7d44eb48e96d8
emmanuel <sunartio.emmanuel@gmail.com>
2023-01-20 10:37:29 +1030

    splash screen

--- app/src/main/java/com/example/marcellinapizzas/MainActivity.kt
+++ app/src/main/java/com/example/marcellinapizzas/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.marcellinapizzas
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
@@ -23,6 +24,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
 
